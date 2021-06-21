f3c3a2363b858aebfbcce5858d31729d3f04b68d
mariana <mari_pnascimento@hotmail.com>
2021-06-21 19:31:19 -0300

    create parser

--- crawlerpjepb.py
+++ crawlerpjepb.py
@@ -1,9 +1,9 @@
 import requests
 
-def get_lawsuit(cnj):
+def get_lawsuit(cnj_number):
 	print('fazendo requisicao do cabecalho do processo')
 
-	clean_number = cnj_cleaner(cnj)
+	clean_number = cnj_cleaner(cnj_number)
 	url_general = f'https://esb.tjpb.jus.br/cp-backend/sistemas/3/processos/{clean_number}?campos=completo'
 	url_activity = f'https://esb.tjpb.jus.br/cp-backend/sistemas/3/processos/{clean_number}/movimentos'
 	
@@ -18,31 +18,34 @@ def get_lawsuit(cnj):
 	'general':response_general.json(),
 	'activity':responde_activity.json()
 	}
-	print(data['general']
-)	#import pdb; pdb.set_trace()
+	print(data['activity'])
+	import pdb; pdb.set_trace()
 	lawsuit = parser(data)
 
 	return lawsuit 
-def cnj_cleaner(cnj):
+def cnj_cleaner(cnj_number):
 	
-	return cnj.replace('-','').replace('.','') 
+	return cnj_number.replace('-','').replace('.','') 
 
 def parser(data):
 	print('filtrando as informacoes')
 
+	general = data['general']
+	activity = data['activity']
 
-	related_people = get_related_people(data['dados']['capa'])
-	activity_list = get_activity_list(data['dados']['movimentos'])
-	basic_info = get_basic_info(data['dados']['cabecalho'])
-	
 	lawsuit = {
-	'basic_info':basic_info,
-	'related_people': related_people,
-	'activity_list': activity_list
+	'number': general['numeroProcesso'],
+	'kind': general['classe'],
+	'court_section': general['orgaoJulgador']['descricao'],
+	'subject': general['assuntoPrincipal']['descricao'],
+	'distribution_date': general['dataDistribuicao'],
+	'value': general['valorAcao'],
+	'status':  general['status']['descricao'],
+	'justice_secret':general['sigiloso'],
+	'related_people':[general['poloAtivo']['nomeParte'],general['poloPassivo']['nomeParte']]
 	}
+
 
 
 print(get_lawsuit('0820115-36.2017.8.15.2001'))
 
-#duvidas:
-#pq o parametro muda do metodo do get_lawsuit e do parser?
